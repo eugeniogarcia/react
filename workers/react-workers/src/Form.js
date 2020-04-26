179f1e9e23d40da80f00db679ed7817a7922da3c
Eugenio Garcia San Martin <egsmartin@gmail.com>
2020-04-27 00:43:39 +0200

    react webworkers

---
+++ workers/react-workers/src/Form.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState(() => {
+      return {
+        value,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label >Estado: </label>
+        <input
+          id="estado"
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+}
+
+export default Form;
--- workers/react-workers/src/index.js
+++ workers/react-workers/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Form from './Form';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Form />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-serviceWorker.register();
+serviceWorker.unregister();
